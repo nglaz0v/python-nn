1bb7825b18a51b58c53ad22230e682db372b6c58
Nikolai <glazov_n_o@mail.ru>
2025-01-02 18:13:36 +0300

    directory structure has been reorganized

--- 4/4.21_GAN-music.py
+++ 4/4.21_GAN-music.py
@@ -3,21 +3,25 @@
 музыкальной генеративной модели с помощью глубокого обучения.
 """
 
+# https://storage.googleapis.com/magentadata/models/music_vae/checkpoints/cat-mel_2bar_big.tar
+
 import magenta.music as mm
 import magenta.models.music_vae as mvae
-# import magenta.music.sequence_generator as sg
-from magenta.models.shared import sequence_generator as sg
 from magenta.models.music_vae import TrainedModel
+from magenta.models.shared import sequence_generator_bundle
+from magenta.models.shared import sequence_generator as sg
+
+BASE_DIR = "gs://download.magenta.tensorflow.org/models/music_vae/colab2"
 
 # Загрузка предобученной модели
 model_name = "cat-mel_2bar_big"
 model_config = mvae.configs.CONFIG_MAP[model_name]
-model_hparams = mvae.trained_model.get_default_hparams(model_name)
-model = TrainedModel(model_name, model_config, model_hparams)
+model = TrainedModel(model_config, batch_size=4,
+                     checkpoint_dir_or_path=BASE_DIR + '/checkpoints/mel_2bar_big.ckpt')
 
 # Создание среды обучения
-mm.notebook_utils.download_bundle(model_name, "bundles")
-bundle = mm.sequence_generator_bundle.read_bundle_file(f"bundles/{model_name}.mag")
+mm.notebook_utils.download_bundle(f"{model_name}.mag", "bundles")
+bundle = sequence_generator_bundle.read_bundle_file(f"bundles/{model_name}.mag")
 generator_map = mm.generator_pb2.GeneratorDetails(id='default', description='')
 generator = sg.BaseSequenceGenerator(model, generator_map)
 
