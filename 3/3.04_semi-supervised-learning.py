bc52a0b0d4f26a64fe7cc2acb647673fb310b767
Глазов Николай Олегович <nglazov@cspfmba.ru>
2025-12-10 14:27:32 +0300

    Fix 3.04_semi-supervised-learning.py

--- 3/3.04_semi-supervised-learning.py
+++ 3/3.04_semi-supervised-learning.py
@@ -1,27 +1,21 @@
 """
-Пример полуобученного обучения с использованием библиотеки scikit-learn на
-Python.
+Пример полуобученного обучения с использованием библиотеки scikit-learn 
+на Python.
 """
 
-from sklearn.linear_model import SGDClassifier
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
 import numpy as np
+from sklearn import datasets
+from sklearn.semi_supervised import LabelPropagation
 
-# Некоторые обучающие данные
-X_train = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
-y_train = np.array([0, 1, 0, 1])
 
-# Некоторые неразмеченные данные
-X_unlabeled = np.array([[1.5, 2.5], [3.5, 4.5]])
+label_prop_model = LabelPropagation()
+iris = datasets.load_iris()
+rng = np.random.RandomState(42)
+random_unlabeled_points = rng.rand(len(iris.target)) < 0.3
+labels = np.copy(iris.target)
+labels[random_unlabeled_points] = -1
+result = label_prop_model.fit(iris.data, labels)
+labels_pred = result.predict(iris.data)
 
-# Создание модели полуобученного обучения (Stochastic Gradient Descent Classifier)
-model = make_pipeline(StandardScaler(),
-                      SGDClassifier(loss='log_loss', max_iter=1000))
-
-# Обучение модели на части размеченных данных
-model.fit(X_train, y_train)
-
-# Прогнозирование меток классов для неразмеченных данных
-predicted_labels = model.predict(X_unlabeled)
-print("Predicted labels for unlabeled data:", predicted_labels)
+print(labels)
+print(labels_pred)
